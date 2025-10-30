973caa58e5e04d96a348388b7267b229dfcd5a37
MellowestMellow <andresjmm1384@icloud.com>
2025-10-30 10:06:10 -0600

    fix: ra rau

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    //IP: 'https://sispruebadesarrollo9.unah.edu.hn/app',
+  IP: 'https://anriserver.duckdns.org', 
+  SITE: 'sigd',
+  API: 'api',
+  INTERNAL: 'internal',
+  PROCESS_ENV: 'dev',
+  MODULE: 'docencia',
+  VERSION : 'v1',
+  secretKey : 'h8As9LtE*f^YzQ2oP3u$G@eVp1Nv!R+k$5Tx!cF&WyZpL9kH',
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  IP: 'https://anriserver.duckdns.org', 
+  SITE: 'sigd',
+  API: 'api',
+  INTERNAL: 'internal',
+  PROCESS_ENV: 'dev',
+  MODULE: 'docencia',
+  VERSION : 'v1',
+  secretKey : 'h8As9LtE*f^YzQ2oP3u$G@eVp1Nv!R+k$5Tx!cF&WyZpL9kH',
+};
+
+//Hola
